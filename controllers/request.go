d43e1b75714235837fdfce7934a168001605384e
Dearlimg <1492568061@qq.com>
2024-11-07 18:52:56 +0800

    优化JWT中间件

--- controllers/request.go
+++ controllers/request.go
@@ -1,9 +1,24 @@
 package controllers
 
 import (
+	"errors"
+	"web-app/middlewares"
+
 	"github.com/gin-gonic/gin"
 )
 
-func GetCurrentUser(c *gin.Context) {
+var ErrorUserNotLogin = errors.New("用户未登录")
 
+func getCurrentUser(c *gin.Context) (userID int64, err error) {
+	uid, ok := c.Get(middlewares.ContextUserIDKey)
+	if !ok {
+		err = ErrorUserNotLogin
+		return
+	}
+	userID, ok = uid.(int64)
+	if !ok {
+		err = ErrorUserNotLogin
+		return
+	}
+	return
 }
